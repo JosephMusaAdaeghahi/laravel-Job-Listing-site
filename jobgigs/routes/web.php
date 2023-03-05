e0a78bdaf14e85f21870b477b45477f4a52c8935
Joseph <adaeghahi@gmail.com>
2023-03-04 23:40:07 -0800

    Final Commit

---
+++ jobgigs/routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+use App\Models\Listing;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//all listing
+Route::get('/', [ListingController::class, 'index']);
+
+
+// create form
+Route::get('/listings/create', [ListingController::class, 'create'])->middleware('auth');
+
+//store listing
+Route::post('/listings',  [ListingController::class, 'store'])->middleware('auth');
+
+//edit form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->middleware('auth');
+
+// update listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
+// delete listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy'])->middleware('auth');
+
+//manage listings
+Route::get('/listings/manage', [ListingController::class, 'manage'])->middleware('auth');
+
+//single listing
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
+//register/create form
+Route::get('/register', [UserController::class, 'create'])->middleware('guest');
+
+//create new user
+Route::post('/users', [UserController::class, 'store']);
+
+//log user out
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
+
+//show log in form
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
+
+//log in user
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
+
+
